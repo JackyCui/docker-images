61543575ab284b3a033315ed4a9aa8a65cfbdca6
cuijuntao <cuijuntao@shgoome.com>
2018-11-29 14:15:04 +0800

    use simple word, add docker-compose other node, use host network

--- kafka-cluster-ssl/secrets/create-certs.sh
+++ kafka-cluster-ssl/secrets/create-certs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 set -o nounset \
     -o errexit \
@@ -6,17 +6,17 @@ set -o nounset \
     -o xtrace
 
 # Generate CA key
-openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 9999 -subj '/CN=ca1.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN' -passin pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -passout pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
-# openssl req -new -x509 -keyout snakeoil-ca-2.key -out snakeoil-ca-2.crt -days 9999 -subj '/CN=ca1.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN' -passin pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -passout pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 9999 -subj '/CN=ca1.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN' -passin pass:yVG1m27J1rtwjdEZ -passout pass:yVG1m27J1rtwjdEZ
+# openssl req -new -x509 -keyout snakeoil-ca-2.key -out snakeoil-ca-2.crt -days 9999 -subj '/CN=ca1.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN' -passin pass:yVG1m27J1rtwjdEZ -passout pass:yVG1m27J1rtwjdEZ
 
 # Kafkacat
-openssl genrsa -des3 -passout "pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" -out kafkacat.client.key 1024
-openssl req -passin "pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" -passout "pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" -key kafkacat.client.key -new -out kafkacat.client.req -subj '/CN=kafkacat.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN'
-openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in kafkacat.client.req -out kafkacat-ca1-signed.pem -days 9999 -CAcreateserial -passin "pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW"
+openssl genrsa -des3 -passout "pass:yVG1m27J1rtwjdEZ" -out kafkacat.client.key 1024
+openssl req -passin "pass:yVG1m27J1rtwjdEZ" -passout "pass:yVG1m27J1rtwjdEZ" -key kafkacat.client.key -new -out kafkacat.client.req -subj '/CN=kafkacat.test.gpswv.com/OU=WW/O=WW/L=Shanghai/S=Sh/C=CN'
+openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in kafkacat.client.req -out kafkacat-ca1-signed.pem -days 9999 -CAcreateserial -passin "pass:yVG1m27J1rtwjdEZ"
 
 
 
-for i in broker1 broker2 broker3 producer consumer
+for i in broker1 broker2 broker3 broker4 broker5 broker6 producer consumer
 do
 	echo $i
 	# Create keystores
@@ -25,22 +25,22 @@ do
 				 -dname "CN=$i.test.gpswv.com, OU=WW, O=WW, L=Shanghai, S=Sh, C=CN" \
 				 -keystore kafka.$i.keystore.jks \
 				 -keyalg RSA \
-				 -storepass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW \
-				 -keypass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+				 -storepass yVG1m27J1rtwjdEZ \
+				 -keypass yVG1m27J1rtwjdEZ
 
 	# Create CSR, sign the key and import back into keystore
-	keytool -keystore kafka.$i.keystore.jks -alias $i -certreq -file $i.csr -storepass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -keypass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+	keytool -keystore kafka.$i.keystore.jks -alias $i -certreq -file $i.csr -storepass yVG1m27J1rtwjdEZ -keypass yVG1m27J1rtwjdEZ
 
-	openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in $i.csr -out $i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+	openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in $i.csr -out $i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:yVG1m27J1rtwjdEZ
 
-	keytool -keystore kafka.$i.keystore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -keypass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+	keytool -keystore kafka.$i.keystore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass yVG1m27J1rtwjdEZ -keypass yVG1m27J1rtwjdEZ
 
-	keytool -keystore kafka.$i.keystore.jks -alias $i -import -file $i-ca1-signed.crt -storepass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -keypass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+	keytool -keystore kafka.$i.keystore.jks -alias $i -import -file $i-ca1-signed.crt -storepass yVG1m27J1rtwjdEZ -keypass yVG1m27J1rtwjdEZ
 
 	# Create truststore and import the CA cert.
-	keytool -keystore kafka.$i.truststore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW -keypass 7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW
+	keytool -keystore kafka.$i.truststore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass yVG1m27J1rtwjdEZ -keypass yVG1m27J1rtwjdEZ
 
-  echo "7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" > ${i}_sslkey_creds
-  echo "7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" > ${i}_keystore_creds
-  echo "7WjCvXcv@!trK@2jPUr@H%RhqkdO3OoW" > ${i}_truststore_creds
+  echo "yVG1m27J1rtwjdEZ" > ${i}_sslkey_creds
+  echo "yVG1m27J1rtwjdEZ" > ${i}_keystore_creds
+  echo "yVG1m27J1rtwjdEZ" > ${i}_truststore_creds
 done
